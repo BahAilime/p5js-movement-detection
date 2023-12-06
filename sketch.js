f24e841cd5d57f9fed66aab18993ef80d73a31cf
bahAilime <emilia.beguin@gmail.com>
2023-12-06 11:08:09 +0100

    création de la fonction getAverageColor

--- sketch.js
+++ sketch.js
@@ -24,4 +24,27 @@ function draw() {
   frame.filter(INVERT)
   tint(255, 40)
   image(frames[0], 0, 0, width, height);
+
+  console.log(getAverageColor().toString())
+}
+
+function getAverageColor() {
+  loadPixels();
+
+  const totalPixels = width * height;
+  let redSum = 0;
+  let greenSum = 0;
+  let blueSum = 0;
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    redSum += pixels[i];
+    greenSum += pixels[i+1];
+    blueSum += pixels[i+2];
+  }
+
+  let avgRed = redSum / totalPixels;
+  let avgGreen = greenSum / totalPixels;
+  let avgBlue = blueSum / totalPixels;
+
+  return color(((avgRed + avgGreen + avgBlue)/3));
 }
