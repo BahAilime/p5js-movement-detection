ce1d19debc3c35cc52738178fbaa75918732be41
bahAilime <emilia.beguin@gmail.com>
2023-12-06 10:39:46 +0100

    récupération du flux vidéo de la webcam

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
+let video;
+
 function setup() {
   createCanvas(400, 400);
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  video.hide();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(color(130, 252, 126));
-  } else {
-    fill(color(252, 126, 126));
-  }
-  rect(0, 0, width, height)
+  image(video.get(), 0, 0, width, height)
 }
